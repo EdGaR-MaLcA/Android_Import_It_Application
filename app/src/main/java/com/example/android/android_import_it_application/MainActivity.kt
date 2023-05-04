39ceab2699a5b7b7ba39b5c717a644b550de76b4
Edgar <u202020371@upc.edu.pe>
2023-05-04 13:04:41 -0500

    feat: vinculate the menu for travelers to the orders

--- app/src/main/java/com/example/android/android_import_it_application/MainActivity.kt
+++ app/src/main/java/com/example/android/android_import_it_application/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.android.android_import_it_application
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_traveler)
+
+        val imgOrder = findViewById<ImageView>(R.id.ivCustomerOrder)
+
+        imgOrder.setOnClickListener {
+            val intent: Intent = Intent(this, OrderActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/android/android_import_it_application/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.android_import_it_application
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.customer_order_traveler)
+
+    }
+}
