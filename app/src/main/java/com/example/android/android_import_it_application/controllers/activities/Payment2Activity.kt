76297bd44f7fa26d7b6b6903f5cd6fef2fe69600
Leonardo Aquino <u20201b949@upc.edu.pe>
2023-05-30 15:47:13 -0500

    Add payment procedure

--- app/src/main/java/com/example/android/android_import_it_application/controllers/activities/Payment2Activity.kt
+++ app/src/main/java/com/example/android/android_import_it_application/controllers/activities/Payment2Activity.kt
@@ -2,6 +2,7 @@ package com.example.android.android_import_it_application.controllers.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.android_import_it_application.R
@@ -13,10 +14,16 @@ class Payment2Activity : AppCompatActivity(){
         setContentView(R.layout.payment2)
 
         val ibArrow3 = findViewById<ImageButton>(R.id.ibArrow3)
+        val bAddTarget2 = findViewById<Button>(R.id.bAddTarget2)
 
         ibArrow3.setOnClickListener {
             val intent: Intent = Intent(this, PaymentActivity::class.java)
             startActivity(intent)
         }
+        bAddTarget2.setOnClickListener {
+            val intent: Intent = Intent(this, Payment3Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
