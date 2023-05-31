168de700d0b944f9fb9e7d19c4e94f56103be536
Edgar <u202020371@upc.edu.pe>
2023-05-30 20:12:48 -0500

    feat: added database management

---
+++ app/src/main/java/com/example/android/android_import_it_application/database/CouponDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.android.android_import_it_application.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android.android_import_it_application.models.Coupon
+
+@Database(entities = [Coupon::class], version = 1)
+abstract class CouponDatabase : RoomDatabase(){
+
+    abstract fun getCouponDAO(): CouponDao
+
+    companion object{ //patron singleton
+        private var INSTANCE : CouponDatabase? = null
+
+        fun getInstance(context: Context): CouponDatabase{
+            if (INSTANCE == null){
+                //la bd no existe
+                INSTANCE = Room
+                    .databaseBuilder(context, CouponDatabase::class.java, "couponsv1.db")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as CouponDatabase
+        }
+    }
+}
