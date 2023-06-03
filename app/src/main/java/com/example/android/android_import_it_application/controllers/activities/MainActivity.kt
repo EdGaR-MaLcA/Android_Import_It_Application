67cc617b3555abcb150b9e361a597315fca54cd5
Edgar <u202020371@upc.edu.pe>
2023-06-03 17:32:39 -0500

    fix: changed name

--- app/src/main/java/com/example/android/android_import_it_application/controllers/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/android_import_it_application/controllers/activities/MainActivity.kt
@@ -7,26 +7,6 @@ import android.widget.Button
 import com.example.android.android_import_it_application.R
 
 class MainActivity : AppCompatActivity() {
-    /*
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-            item -> navigateTo(item)
-    }
-
-    private fun navigateTo(item: MenuItem) : Boolean{
-        item.isChecked = true
-        return supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.flFragment, getFragmentFor(item))
-            .commit() > 0
-    }
-
-    private fun getFragmentFor(item: MenuItem): Fragment {
-        return when (item.itemId){
-            R.id.menu_home -> SeeProductsFragment()
-            else -> CouponFragment()
-        }
-    }
-*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,42 +19,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_traveler)
-
-        val imgOrder = findViewById<ImageView>(R.id.ivCustomerOrder)
-        val tvCustomerOrder = findViewById<TextView>(R.id.tvCustomerOrder)
-        val ivWallet = findViewById<ImageView>(R.id.ivWallet)
-        val tvWallet = findViewById<TextView>(R.id.tvWallet)
-        //val tvBackWallet = findViewById<TextView>(R.id.textView40)
-
-        imgOrder.setOnClickListener {
-            val intent: Intent = Intent(this, OrderActivity::class.java)
-            startActivity(intent)
-        }
-
-        tvCustomerOrder.setOnClickListener {
-            val intent: Intent = Intent(this, OrderActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
-        tvWallet.setOnClickListener {
-            val intent: Intent = Intent(this, WalletActivity::class.java);
-            startActivity(intent)
-        }
-
-        ivWallet.setOnClickListener {
-            val intent: Intent = Intent(this, WalletActivity::class.java);
-            startActivity(intent)
-        }
-
-        /*tvBackWallet.setOnClickListener {
-            val intent: Intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }*/
-    }*/
 
 }
