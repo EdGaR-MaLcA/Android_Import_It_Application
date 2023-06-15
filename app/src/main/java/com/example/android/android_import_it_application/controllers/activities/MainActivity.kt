627757b96bfa2056cb0c922e8af6fa25e4f35719
JackelineSalome <89036070+JackelineSalome@users.noreply.github.com>
2023-06-15 15:33:06 -0500

    add improvements

--- app/src/main/java/com/example/android/android_import_it_application/controllers/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/android_import_it_application/controllers/activities/MainActivity.kt
@@ -3,28 +3,43 @@ package com.example.android.android_import_it_application.controllers.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.example.android.android_import_it_application.R
+import com.example.android.android_import_it_application.controllers.activities.SignIn.LogIn
 import com.example.android.android_import_it_application.controllers.activities.SignIn.SelectRole
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_select_role)
 
-        val btBuyer = findViewById<Button>(R.id.btBuyer)
-        val btTraveler = findViewById<Button>(R.id.btTraveler)
-
-        btBuyer.setOnClickListener{
-            val intent: Intent = Intent(this, SelectRole::class.java)
-            startActivity(intent)
+        val ivBuyer = findViewById<View>(R.id.ivBuyer)
+        val ivTraveler = findViewById<View>(R.id.ivTraveler)
+        val tvComprador = findViewById<TextView>(R.id.tvComprador)
+        val tvViajero = findViewById<TextView>(R.id.tvViajero)
+        ivBuyer.setOnClickListener {
+            startLogInActivity("buyer")
         }
 
-        btTraveler.setOnClickListener{
-            val intent: Intent = Intent(this, SelectRole::class.java)
-            startActivity(intent)
+        tvComprador.setOnClickListener{
+            startLogInActivity("buyer")
         }
 
+        ivTraveler.setOnClickListener {
+            startLogInActivity("traveler")
+        }
+
+        tvViajero.setOnClickListener {
+            startLogInActivity("traveler")
+        }
+
+    }
+    private fun startLogInActivity(role: String) {
+        val intent = Intent(this, LogIn::class.java)
+        intent.putExtra("role", role)
+        startActivity(intent)
     }
 
 }
