f5908bb5b645b8e19139c089fa5672ed0414629e
Edgar <u202020371@upc.edu.pe>
2023-06-15 12:57:20 -0500

    fix: added place order activity

---
+++ app/src/main/java/com/example/android/android_import_it_application/controllers/activities/PlaceOrderForm.kt
@@ -0,0 +1,12 @@
+package com.example.android.android_import_it_application.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.android_import_it_application.R
+
+class PlaceOrderForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_order_form)
+    }
+}
